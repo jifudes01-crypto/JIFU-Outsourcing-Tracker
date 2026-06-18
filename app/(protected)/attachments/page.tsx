e516576b7c20d32f3c591410aa0b4599ee30616f
JIFU設計 <jifu.des01@gmail.com>
2026-06-18 11:32:03 +0800

    Add project files

---
+++ app/(protected)/attachments/page.tsx
@@ -0,0 +1,14 @@
+import { OrdersClient } from "@/components/orders-client";
+import { getCurrentProfile } from "@/lib/auth";
+import { getCategories, getOrders, getProfiles, getVendors } from "@/lib/data";
+
+export default async function AttachmentsPage() {
+  const [{ profile }, orders, vendors, categories, profiles] = await Promise.all([
+    getCurrentProfile(),
+    getOrders(),
+    getVendors(),
+    getCategories(),
+    getProfiles()
+  ]);
+  return <OrdersClient initialOrders={orders} vendors={vendors} categories={categories} profiles={profiles} profile={profile} />;
+}
